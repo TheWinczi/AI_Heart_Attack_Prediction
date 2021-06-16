3e96831f8beefd28b6aad3fe57b3d9785164d0e7
TheWinczi <mwinczewski@tutanota.com>
2021-06-16 16:07:39 +0200

    Processing data using importances was implemented. Some data diagrams was added. Project was ended

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ from data_manager import *
 from classifiers import *
 
 
+def lunch_classifiers(x_train, x_test, y_train, y_test):
+    tree = decision_tree(x_train, x_test, y_train, y_test)
+    forest = random_forest(x_train, x_test, y_train, y_test)
+    knn = knn_classifier(x_train, x_test, y_train, y_test)
+    svc = svc_classifier(x_train, x_test, y_train, y_test)
+    return tree, forest, knn, svc
+
 
 def main():
     data = load_data()
     x_train, x_test, y_train, y_test = process_data(data)
 
-    decision_tree(x_train, x_test, y_train, y_test)
-    random_forest(x_train, x_test, y_train, y_test)
-    knn_classifier(x_train, x_test, y_train, y_test)
-    svc_classifier(x_train, x_test, y_train, y_test)
+    print("Before significance filter:")
+    tree, forest, knn, svc = lunch_classifiers(x_train, x_test, y_train, y_test)
+
+    gamma = 0.75
+    x_train, x_test, y_train, y_test = process_data_using_importances(data, gamma, forest)
+
+    print("\nAfter significance filter:")
+    lunch_classifiers(x_train, x_test, y_train, y_test)
 
 
 if __name__ == "__main__":
